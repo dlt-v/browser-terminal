3be3798b9710947cc56cb3021a1a05d1a9a60eb1
Delta.v <tmm.michalski@gmail.com>
2022-07-04 00:40:04 +0200

    feat: add boot sequence

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-const overlay = document.querySelector('.scanlines');
-const input = document.querySelector('.input');
-const lineInput = document.querySelector('.line-input');
-const lineContainer = document.querySelector('.line-container');
-let isPLaying = false;
 const sfx = {
     click: new Howl({
         src: ['audio/click.mp3'],
@@ -29,58 +24,98 @@ const sfx = {
         volume: 0.5,
     }),
 };
-
 const clicks = [sfx.click, sfx.click2, sfx.click3, sfx.click4, sfx.click5];
-input.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        console.log('enter');
-        generateText(input.value, sampleText);
-        input.value = '';
-    }
-});
 
-overlay.addEventListener('click', () => {
-    input.focus();
-    if (!isPLaying) {
-        let sound = new Howl({
-            src: ['audio/computer_hum.mp3'],
+class Terminal {
+    constructor() {
+        this.status = 'startup';
+        this.overlay = document.querySelector('.scanlines');
+        this.input = document.querySelector('.input');
+        this.lineInput = document.querySelector('.line-input');
+        this.lineContainer = document.querySelector('.line-container');
+        this.prefix = '>';
+        //don't allow interaction when terminal is printing something
+        this.takesAction = false;
+        this.humPlaying = false;
+    }
+
+    async generateLine(text) {
+        //Generate one line for the terminal
+        let line = document.createElement('div');
+        line.classList.add('line');
+        this.lineContainer.appendChild(line);
+        for (let i = 0; i < text.length; i++) {
+            this.playClick();
+            line.innerHTML += text[i];
+            await this.sleep(50);
+        }
+    }
+    addLine(text, isCommand) {
+        let line = document.createElement('pre');
+        line.classList.add('line');
+        line.innerHTML = isCommand ? this.prefix : '';
+        line.innerHTML += text;
+        this.lineContainer.appendChild(line);
+        return line;
+    }
+    onEnter() {
+        this.input.style.opacity = '0';
+        this.lineInput.classList.remove('line-input-before');
+        this.addLine(this.input.value, true);
+        this.input.value = '';
+        this.generateLine('Hello!');
+        this.input.style.opacity = '1';
+        this.lineInput.classList.add('line-input-before');
+    }
+    cleanLineContainer() {
+        document.querySelectorAll('.line').forEach((element) => {
+            element.remove();
         });
-        sound.play();
-        sound.fade(0.0, 0.8, 1000);
-        sound.loop(true);
-        isPLaying = true;
+    }
+
+    async sleep(ms) {
+        //Stop the program for a moment
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    playClick() {
+        //Play the click sound
+        let click_id = Math.floor(Math.random() * 5);
+        clicks[click_id].play();
+    }
+    playBoop() {
+        //Play boop at the end of the sequence
+        sfx.boop.play();
+    }
+    finishSequence() {
+        this.lineInput.classList.add('line-input-before');
+        terminal.lineInput.style.opacity = '1';
+    }
+}
+
+terminal = new Terminal();
+
+terminal.input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        if (terminal.takesAction) return;
+        terminal.lineInput.classList.remove('line-input-before');
+        terminal.lineInput.style.opacity = '0';
+        switch (terminal.status) {
+            case 'startup':
+                terminal.takesAction = true;
+                startSequence(terminal);
+                break;
+            case 'bootup':
+                terminal.takesAction = true;
+                bootUpSequence(terminal);
+                break;
+            default:
+                terminal.onEnter();
+                break;
+        }
     }
 });
-const sampleText =
-    'Nulla facilisi. Vivamus ultrices in eros et rhoncus. Aenean odio nulla, hendrerit id molestie quis, tempus sed leo. Donec pellentesque eget odio in maximus. Donec tristique hendrerit lobortis. Nullam porttitor turpis et mi fermentum facilisis. Etiam et orci non risus rhoncus lobortis in eget dolor. Duis sit amet massa vel quam dapibus pulvinar sed et dolor. Quisque luctus neque ac enim elementum ornare. Integer aliquam aliquet facilisis.';
 
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-const generateText = (inputValue, text) => {
-    //print current command
-    let line = document.createElement('div');
-    line.classList.add('line');
-    line.innerText = `G:\\dev>${inputValue}`;
-    lineContainer.appendChild(line);
-
-    //add new html element
-    line = document.createElement('div');
-    line.classList.add('line');
-    let lineText = 'General Kenobi!';
-    lineContainer.appendChild(line);
-    lineByLine(line, lineText);
-};
-const lineByLine = async (element, text) => {
-    input.style.opacity = '0';
-    lineInput.classList.remove('line-input-before');
-    for (let i = 0; i < text.length; i++) {
-        click_id = Math.floor(Math.random() * 5);
-        clicks[click_id].play();
-        element.innerHTML += text[i];
-        await sleep(50);
-    }
-    sfx.boop.play();
-    input.style.opacity = '1';
-    lineInput.classList.add('line-input-before');
-};
+terminal.overlay.addEventListener('click', () => {
+    //Play computer hum when starting the program
+    terminal.input.focus();
+});
