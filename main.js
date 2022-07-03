f434f7991c3e060a34214939ab8060a5531a53e2
Delta.v <tmm.michalski@gmail.com>
2022-07-03 12:25:49 +0200

    import Howl.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const overlay = document.querySelector('.scanlines');
 const input = document.querySelector('.input');
 let isPLaying = false;
-let audio = new Audio('audio/hum.mp3');
+
 overlay.addEventListener('click', () => {
     input.focus();
     if (!isPLaying) {
-        audio.play();
+        let sound = new Howl({
+            src: ['audio/hum.mp3'],
+        });
+        sound.play();
     }
 });
